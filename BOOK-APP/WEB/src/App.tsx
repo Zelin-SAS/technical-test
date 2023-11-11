6d61ce80a67e285d7dc5330d13c8e43d2dd0af84
Taib <taibamjoud5@gmail.com>
2023-11-11 12:58:51 +0100

    Implement dark mode

--- BOOK-APP/WEB/src/App.tsx
+++ BOOK-APP/WEB/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     base: `"nav" " aside"`,
     lg: `"nav nav" "aside main"`
   }}>
-    <GridItem area="nav" bg="coral">
+    <GridItem area="nav">
       <NavBar />
     </GridItem>
     <GridItem area="aside" bg="blue">aside</GridItem>
