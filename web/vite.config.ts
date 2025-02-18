bbad8774994d79ff24853732889add21896e02e2
asthino <zafiamy2000asthino@gmail.com>
2025-02-18 23:48:48 +0100

    [E] - remove logs

---
+++ web/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv'
+import tailwindcss from '@tailwindcss/vite';
+
+dotenv.config();
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+     usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: parseInt(process.env.FRONTEND_PORT || '5173'),
+  },
+})
